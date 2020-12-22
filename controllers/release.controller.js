aa265f8f439e33a8c952219139ab49e1ac22ca0a
Thor Stenbæk <tst@dips.no>
2020-12-22 20:49:24 +0100

    Added check for existing namespace

--- controllers/release.controller.js
+++ controllers/release.controller.js
@@ -24,72 +24,72 @@ exports.createRelease = async (req, res) => {
     let app = k8s.getAppApi();
     let networking = k8s.getNetworkingApi();
 
-    for (const spec of validSpecs) {
-        // this is to convince the old version of TypeScript that metadata exists even though we already filtered specs
-        // without metadata out
-        spec.metadata = spec.metadata || {};
-        spec.metadata.annotations = spec.metadata.annotations || {};
-        delete spec.metadata.annotations['kubectl.kubernetes.io/last-applied-configuration'];
-        spec.metadata.annotations['kubectl.kubernetes.io/last-applied-configuration'] = JSON.stringify(spec);
-        
-        try {
-            // try to get the resource, if it does not exist an error will be thrown and we will end up in the catch
-            // block.
+    try {
+        var namespaceObject = await core.readNamespace(namespace);    
+
+        for (const spec of validSpecs) {
+            // this is to convince the old version of TypeScript that metadata exists even though we already filtered specs
+            // without metadata out
+            spec.metadata = spec.metadata || {};
+            spec.metadata.annotations = spec.metadata.annotations || {};
+            delete spec.metadata.annotations['kubectl.kubernetes.io/last-applied-configuration'];
+            spec.metadata.annotations['kubectl.kubernetes.io/last-applied-configuration'] = JSON.stringify(spec);
             
-            if (spec.kind == "Deployment")
-            {
-                var deployment = await app.readNamespacedDeployment(spec.metadata.name, namespace);
-                console.log("Found deployment: " + deployment.metadata.name);
-            }
-            else if (spec.kind == "Service")
-            {
-                var service = await core.readNamespacedService(spec.metadata.name, namespace);    
-                console.log("Found service: " + service.metadata.name);
-            }
-            else if (spec.kind == "Ingress")
-            {
-                var ingress = await networking.readNamespacedIngress(spec.metadata.name, namespace);
-                console.log("Found ingress: " + ingress.metadata.name);
-            }
-            
-            // we got the resource, so it exists, so patch it
-            
-            /*if (spec.kind == "Deployment")
-            {
-                const deployment = await app.patchNamespacedDeployment(spec.metadata.name, namespace, spec);
-                created.push(deployment.body);
-            }
-            else if (spec.kind == "Service")
-            {
-                const service = await core.patchNamespacedService(spec.metadata.name, namespace, spec);
-                created.push(service.body);
-            }
-            else if (spec.kind == "Ingress")
-            {
-                const ingress = await app.patchNamespacedIngress(spec.metadata.name, namespace, spec);
-                created.push(ingress.body);
-            }*/
-        } catch (e) {
-            // we did not get the resource, so it does not exist, so create it
-            if (spec.kind == "Deployment")
-            {
-                const deployment = await app.createNamespacedDeployment(namespace, spec);
-                created.push(deployment.body);
-            }
-            else if (spec.kind == "Service")
-            {
-                const service = await core.createNamespacedService(namespace, spec);
-                created.push(service.body);
-            }
-            else if (spec.kind == "Ingress")
-            {
-                const ingress = await networking.createNamespacedIngress(namespace, spec);
-                created.push(ingress.body);
+            try {
+                // try to get the resource, if it does not exist an error will be thrown and we will end up in the catch
+                // block.
+                
+                if (spec.kind == "Deployment")
+                {
+                    var deployment = await app.readNamespacedDeployment(spec.metadata.name, namespace);
+                    console.log("Found deployment: " + deployment.metadata.name);
+    
+                    deployment = await app.patchNamespacedDeployment(spec.metadata.name, namespace, spec);
+                    created.push(deployment.body);
+                }
+                else if (spec.kind == "Service")
+                {
+                    var service = await core.readNamespacedService(spec.metadata.name, namespace);    
+                    console.log("Found service: " + service.metadata.name);
+    
+                    service = await core.patchNamespacedService(spec.metadata.name, namespace, spec);
+                    created.push(service.body);
+                }
+                else if (spec.kind == "Ingress")
+                {
+                    var ingress = await networking.readNamespacedIngress(spec.metadata.name, namespace);
+                    console.log("Found ingress: " + ingress.metadata.name);
+    
+                    ingress = await app.patchNamespacedIngress(spec.metadata.name, namespace, spec);
+                    created.push(ingress.body);
+                }                        
+            } catch (e) {
+                // we did not get the resource, so it does not exist, so create it
+                if (spec.kind == "Deployment")
+                {
+                    const deployment = await app.createNamespacedDeployment(namespace, spec);
+                    created.push(deployment.body);
+                }
+                else if (spec.kind == "Service")
+                {
+                    const service = await core.createNamespacedService(namespace, spec);
+                    created.push(service.body);
+                }
+                else if (spec.kind == "Ingress")
+                {
+                    const ingress = await networking.createNamespacedIngress(namespace, spec);
+                    created.push(ingress.body);
+                }
             }
         }
+    } catch (error) {
+        const message = `Namespace ${namespace} was not found`;
+        console.error(message);
+        res.send(message);
+        return;
     }
     
-    res.send(created);
+    res.send(created);    
 }
 
 exports.getRelese = async (req, res) => {
